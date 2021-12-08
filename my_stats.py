11cc37ba0f61a38f586884cd869a694c232a2362
hDmtP <pritamdhara914@gmail.com>
2021-12-08 12:56:08 +0530

    automated scoring workflow

---
+++ my_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from github_contributions import GithubUser
+from datetime import datetime, date
+import pytz
+
+tz_NY = pytz.timezone('Asia/Kolkata')
+datetime_NY = datetime.now(tz_NY)
+print(datetime_NY)
+
+user = GithubUser('hdmtp')
+contribs = user.contributions()
+
+contribs_2021 = user.contributions(
+    start_date='2021-11-30', end_date=str(date.today()))
+
+sc = f"\n**{datetime_NY}** | **{sum([day.count for day in contribs_2021.days])}**"
+
+f = open("README.md", "a")
+f.write(sc)
+f.close()
